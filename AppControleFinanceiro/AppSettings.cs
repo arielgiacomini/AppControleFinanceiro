fa48ab41f3d79d9f296d40a37a85a232f5cba473
Ariel Giacomini <giacomini.ariel@gmail.com>
2024-02-10 17:45:40 -0300

    aula 22.14 ok

---
+++ AppControleFinanceiro/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AppControleFinanceiro
+{
+    public static class AppSettings
+    {
+        private static readonly string DatabaseName = "database.db";
+        private static readonly string DatabaseDirectory = FileSystem.AppDataDirectory;
+        public static readonly string DatabasePath = Path.Combine(DatabaseDirectory, DatabaseName);
+    }
+}
