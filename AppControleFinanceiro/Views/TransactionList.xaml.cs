74f83232da1260e2e8f0ba92150bf956eb0927da
Ariel Giacomini <arieltecnologia@outlook.com>
2024-02-16 20:29:26 -0300

    Finalizando a aula 40.33

--- AppControleFinanceiro/Views/TransactionList.xaml.cs
+++ AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -1,9 +1,16 @@
 using AppControleFinanceiro.Repositories;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
+    /*
+     * Publisher - Subriscribers ->
+     * TransactionAdd -> Publisher > Cadastro (Mensagem > Transaction)
+     * OK - Subscribers -> TransactionList (Receba o Transaction). 
+    */
+
     private readonly ITransactionRepository _transactionRepository;
 
     public TransactionList(ITransactionRepository transactionRepository)
@@ -12,12 +19,24 @@ public partial class TransactionList : ContentPage
 
         InitializeComponent();
 
+        Reload();
+
+        WeakReferenceMessenger.Default.Register<string>(this, (e, msg) =>
+        {
+            Reload();
+        });
+    }
+
+    private void Reload()
+    {
         CollectionViewTransactions.ItemsSource = _transactionRepository.GetAll();
     }
 
     private void OnButtonPlusClickedToTransactionAdd(object sender, EventArgs eventArgs)
     {
+
         var transactionAdd = Handler.MauiContext.Services.GetService<TransactionAdd>();
+
         Navigation.PushModalAsync(transactionAdd);
     }
 
