cdc3c47eddc02b1ada24abfa796b6fe8d5a33f37
Ariel Giacomini <arieltecnologia@outlook.com>
2024-02-16 19:37:42 -0300

    Finalizado aula 39.31

--- AppControleFinanceiro/Views/TransactionList.xaml.cs
+++ AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -1,15 +1,25 @@
+using AppControleFinanceiro.Repositories;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
     private readonly TransactionAdd _transactionAdd;
     private readonly TransactionEdit _transactionEdit;
+    private readonly ITransactionRepository _transactionRepository;
 
-    public TransactionList(TransactionAdd transactionAdd, TransactionEdit transactionEdit)
+    public TransactionList(
+        TransactionAdd transactionAdd,
+        TransactionEdit transactionEdit,
+        ITransactionRepository transactionRepository)
     {
-        InitializeComponent();
         _transactionAdd = transactionAdd;
         _transactionEdit = transactionEdit;
+        _transactionRepository = transactionRepository;
+
+        InitializeComponent();
+
+        CollectionViewTransactions.ItemsSource = _transactionRepository.GetAll();
     }
 
     private void OnButtonPlusClickedToTransactionAdd(object sender, EventArgs eventArgs)
